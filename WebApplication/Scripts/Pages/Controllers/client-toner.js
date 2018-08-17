d8826b9feba265f77ca8ff68e107663f1c0e9292
Shadman Kudchikar <shadmanssk@yahoo.in>
2018-08-17 17:47:54 +0530

    remove front end for api web application

--- WebApplication/Scripts/Pages/Controllers/client-toner.js
+++
@@ -1,9 +0,0 @@
-﻿angular.module('dolphinApp')
-    .controller('ClientTonerController', ['$scope', 'ClientService', 'TonerService', function ($scope, $clientService, $tonerService) {
-        $scope.client = null;
-        $scope.all = [];
-        $clientService.onClientChange($scope, function () {
-            $scope.client = $clientService.client;
-            $scope.all = $tonerService.get($scope.client);
-        });
-    }]);
